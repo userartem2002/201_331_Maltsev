d7a7de0cc54536081e0234a0cc8eef415f336327
userartem2002 <96133767+userartem2002@users.noreply.github.com>
2023-11-10 17:54:08 +0300

    first

--- 201_331_Maltsev/mainwindow.h
+++ 201_331_Maltsev/mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "QSignalMapper"
+#include "QListWidgetItem"
+#include "QVBoxLayout"
+#include "QPushButton"
+#include "QMessageBox"
+#include "QLabel"
+#include "QLineEdit"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +22,23 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    //Переменные для окна ввода пин кода
+    QWidget *pin_window;
+    QLineEdit *input_pincode;
+    void show_pin_check_window();//Функция показа окна для ввода pincode
+    QVector<QVector<QString>> hero_info;
+    int readJsonFile(const QString &filename);
+    QString salt = "12345";
+
+    int show_game_window();
+    int add_to_window(int current_card);
+    int current_card = 0;
+private slots:
+    int check_pin_code();
+    void on_pushButton_next_clicked();
+
+    void on_pushButton_back_clicked();
 
 private:
     Ui::MainWindow *ui;
