8d2a7f8defbf8a94344f4ddbf7cc244446344af7
userartem2002 <96133767+userartem2002@users.noreply.github.com>
2023-11-10 17:54:08 +0300

    first

--- 201_331_Maltsev/mainwindow.cpp
+++ 201_331_Maltsev/mainwindow.cpp
@@ -1,5 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QPushButton"
+#include "QMessageBox"
+#include "QRandomGenerator"
+
+#include "QByteArray"
+#include "QFile"
+#include "QJsonDocument"
+#include "QJsonObject"
+
+#include "QDebug"
+#include "QByteArray"
+#include "QBuffer"
+#include "QFile"
+#include "QJsonParseError"
+#include "QJsonObject"
+#include "QJsonArray"
+#include "QDir"
+
+#include "QCryptographicHash"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +33,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::show_pin_check_window() {
+
+    pin_window = new QWidget(); //Создание виджета как окна для ввода пин кода
+    pin_window->setGeometry(683,350,270,120);
+
+    QVBoxLayout *layout_pin = new QVBoxLayout;
+
+    QLabel *pin_code_label = new QLabel("Пин код"); //Лейбл для пинкода
+    input_pincode = new QLineEdit(); // Инпут для пинкода
+    input_pincode->setEchoMode(QLineEdit::Password);
+
+    QPushButton *check_pin_btn = new QPushButton("Вход"); //Кнопка для проверки
+    connect(check_pin_btn, SIGNAL(clicked()), this, SLOT(check_pin_code())); //При нажатии на кнопку отправляемся в функцию проверки
+
+    //Добавляем в окно лейбл кнопку и инпут
+    layout_pin->addWidget(pin_code_label);
+    layout_pin->addWidget(input_pincode);
+    layout_pin->addWidget(check_pin_btn);
+    pin_window->setLayout(layout_pin);
+
+    pin_window->show();
+}
